b3f593cd4a7cf5628132ca22c2f0e4cc94e283eb
Winnie Liu <acs107102@gm.ntcu.edu.tw>
2025-01-09 21:07:05 +0000

    query analysis

--- codeRAG.py
+++ codeRAG.py
@@ -13,6 +13,9 @@ from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
 
+from typing import Literal
+from typing_extensions import Annotated
+
 OPENAI_API_KEY = ""
 WEBSITE = "https://bwfbadminton.com/"
 LANGCHAIN_API_KEY = ""
@@ -47,18 +50,53 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20
 all_splits = text_splitter.split_documents(docs)
 print("Split the document into sub-documents", len(all_splits))
 
+# query analysis
+total_documents = len(all_splits)
+third = total_documents // 3
+
+for i, document in enumerate(all_splits):
+    if i < third:
+        document.metadata["section"] = "beginning"
+    elif i < 2 * third:
+        document.metadata["section"] = "middle"
+    else:
+        document.metadata["section"] = "end"
+
+# storing documents
 _ = vector_store.add_documents(documents=all_splits)
 
+# check langchain prompt hub
 prompt = hub.pull("rlm/rag-prompt")
+
+# schema for search query
+class Search(TypedDict):
+    """Search query."""
+
+    query: Annotated[str, ..., "Search query to run."]
+    section: Annotated[
+        Literal["beginning", "middle", "end"],
+        ...,
+        "Section to query.",
+    ]
 
 # the data to put inside the application
 class State(TypedDict):
     question: str
+    query: Search
     context: List[Document]
     answer: str
 
+def analyze_query(state: State):
+    structured_llm = llm.with_structured_output(Search)
+    query = structured_llm.invoke(state["question"])
+    return {"query": query}
+
 def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
+    query = state["query"]
+    retrieved_docs = vector_store.similarity_search(
+        query["query"],
+        filter=lambda doc: doc.metadata.get("section") == query["section"],
+    )
     return {"context": retrieved_docs}
 
 
@@ -69,11 +107,12 @@ def generate(state: State):
     return {"answer": response.content}
 
 # the control flow
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
+graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
+graph_builder.add_edge(START, "analyze_query")
 graph = graph_builder.compile()
 
-response = graph.invoke({"question": "What is Task Decomposition?"})
+QUESTION = "What does the end of the post say about Task Decomposition?"
+response = graph.invoke({"question": QUESTION})
 print("Answer: ", response["answer"])
 
 # # alternative
