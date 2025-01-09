1e82b70cab702874b68830450996725a84183047
Winnie Liu <acs107102@gm.ntcu.edu.tw>
2025-01-09 21:36:13 +0000

    tool calling

--- codeRAG.py
+++ codeRAG.py
@@ -7,11 +7,15 @@ from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import START, StateGraph
+from langgraph.graph import START, StateGraph, MessagesState
 from typing_extensions import List, TypedDict
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+from langgraph.graph import END
+from langgraph.prebuilt import ToolNode, tools_condition
 
 from typing import Literal
 from typing_extensions import Annotated
@@ -62,7 +66,7 @@ for i, document in enumerate(all_splits):
     else:
         document.metadata["section"] = "end"
 
-# storing documents
+# storing documents - index chunks
 _ = vector_store.add_documents(documents=all_splits)
 
 # check langchain prompt hub
@@ -79,41 +83,87 @@ class Search(TypedDict):
         "Section to query.",
     ]
 
-# the data to put inside the application
-class State(TypedDict):
-    question: str
-    query: Search
-    context: List[Document]
-    answer: str
-
-def analyze_query(state: State):
-    structured_llm = llm.with_structured_output(Search)
-    query = structured_llm.invoke(state["question"])
-    return {"query": query}
-
-def retrieve(state: State):
-    query = state["query"]
-    retrieved_docs = vector_store.similarity_search(
-        query["query"],
-        filter=lambda doc: doc.metadata.get("section") == query["section"],
+@tool(response_format="content_and_artifact") # tool-calling: model call system response
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in retrieved_docs
     )
-    return {"context": retrieved_docs}
+    return serialized, retrieved_docs
 
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = llm.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
 
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
+
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = llm.invoke(prompt)
+    return {"messages": [response]}
 
 # the control flow
-graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
-graph_builder.add_edge(START, "analyze_query")
+graph_builder = StateGraph(MessagesState)
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
 graph = graph_builder.compile()
 
-QUESTION = "What does the end of the post say about Task Decomposition?"
-response = graph.invoke({"question": QUESTION})
-print("Answer: ", response["answer"])
+# question
+input_message = "What is Task Decomposition?"
+
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
 
 # # alternative
 # question = "..."
