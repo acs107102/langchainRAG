82cb3794a3ef74e42ca079fffd32543f940f8231
Winnie Liu <acs107102@gm.ntcu.edu.tw>
2025-01-10 22:24:18 +0000

    Badminton Wiki + Query Analysis

--- codeRAG.py
+++ codeRAG.py
@@ -1,10 +1,13 @@
 import getpass
 import os
 import bs4
+import sys
+import io
 os.environ['USER_AGENT'] = 'myagent' # langchain_community need website / brower variable
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 from langchain import hub
-from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import WebBaseLoader, WikipediaLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langgraph.graph import START, StateGraph, MessagesState
@@ -23,7 +26,7 @@ from typing import Literal
 from typing_extensions import Annotated
 
 OPENAI_API_KEY = ""
-WEBSITE = "https://bwfbadminton.com/"
+WEBSITE = "https://en.wikipedia.org/wiki/BWF_World_Ranking"
 LANGCHAIN_API_KEY = ""
 
 # os.environ["LANGCHAIN_TRACING_V2"] = "true"
@@ -38,17 +41,12 @@ vector_store = InMemoryVectorStore(embeddings)
 
 # loading documents
 loader = WebBaseLoader(
-    # web_path=(WEBSITE,),
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs=dict(
-        parse_only=bs4.SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
-        )
-    ),
+    web_paths=(WEBSITE,),
 )
 
 docs = loader.load()
 assert len(docs) == 1
+print(len(docs))
 print(f"Total characters: {len(docs[0].page_content)}")
 
 # splitting documents
@@ -85,105 +83,39 @@ class Search(TypedDict):
         "Section to query.",
     ]
 
-@tool(response_format="content_and_artifact") # tool-calling: model call system response
-def retrieve(query: str):
-    """Retrieve information related to a query."""
-    retrieved_docs = vector_store.similarity_search(query, k=2)
-    serialized = "\n\n".join(
-        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
-        for doc in retrieved_docs
+# the data to put inside the application
+class State(TypedDict):
+    question: str
+    query: Search
+    context: List[Document]
+    answer: str
+
+def analyze_query(state: State):
+    structured_llm = llm.with_structured_output(Search)
+    query = structured_llm.invoke(state["question"])
+    return {"query": query}
+
+def retrieve(state: State):
+    query = state["query"]
+    retrieved_docs = vector_store.similarity_search(
+        query["query"],
+        filter=lambda doc: doc.metadata.get("section") == query["section"],
     )
-    return serialized, retrieved_docs
+    return {"context": retrieved_docs}
 
-# Step 1: Generate an AIMessage that may include a tool-call to be sent.
-def query_or_respond(state: MessagesState):
-    """Generate tool call for retrieval or respond."""
-    llm_with_tools = llm.bind_tools([retrieve])
-    response = llm_with_tools.invoke(state["messages"])
-    # MessagesState appends messages to state instead of overwriting
-    return {"messages": [response]}
 
-# Step 2: Execute the retrieval.
-tools = ToolNode([retrieve])
-
-# Step 3: Generate a response using the retrieved content.
-def generate(state: MessagesState):
-    """Generate answer."""
-    # Get generated ToolMessages
-    recent_tool_messages = []
-    for message in reversed(state["messages"]):
-        if message.type == "tool":
-            recent_tool_messages.append(message)
-        else:
-            break
-    tool_messages = recent_tool_messages[::-1]
-
-    # Format into prompt
-    docs_content = "\n\n".join(doc.content for doc in tool_messages)
-    system_message_content = (
-        "You are an assistant for question-answering tasks. "
-        "Use the following pieces of retrieved context to answer "
-        "the question. If you don't know the answer, say that you "
-        "don't know. Use three sentences maximum and keep the "
-        "answer concise."
-        "\n\n"
-        f"{docs_content}"
-    )
-    conversation_messages = [
-        message
-        for message in state["messages"]
-        if message.type in ("human", "system")
-        or (message.type == "ai" and not message.tool_calls)
-    ]
-    prompt = [SystemMessage(system_message_content)] + conversation_messages
-
-    # Run
-    response = llm.invoke(prompt)
-    return {"messages": [response]}
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
 
 # the control flow
-graph_builder = StateGraph(MessagesState)
-graph_builder.add_node(query_or_respond)
-graph_builder.add_node(tools)
-graph_builder.add_node(generate)
-
-graph_builder.set_entry_point("query_or_respond")
-graph_builder.add_conditional_edges(
-    "query_or_respond",
-    tools_condition,
-    {END: END, "tools": "tools"},
-)
-graph_builder.add_edge("tools", "generate")
-graph_builder.add_edge("generate", END)
-
+graph_builder = StateGraph(State).add_sequence([analyze_query, retrieve, generate])
+graph_builder.add_edge(START, "analyze_query")
 graph = graph_builder.compile()
 
-# for history
-memory = MemorySaver()
-graph = graph_builder.compile(checkpointer=memory)
-
-# agent: make decision during execution
-agent_executor = create_react_agent(llm, [retrieve], checkpointer=memory)
-
-# Specify an ID for the thread
-config = {"configurable": {"thread_id": "def234"}}
-
-input_message = (
-    "What is the standard method for Task Decomposition?\n\n"
-    "Once you get the answer, look up common extensions of that method."
-)
-
-for event in agent_executor.stream(
-    {"messages": [{"role": "user", "content": input_message}]},
-    stream_mode="values",
-    config=config,
-):
-    event["messages"][-1].pretty_print()
-
-# # alternative
-# question = "..."
-
-# retrieved_docs = vector_store.similarity_search(question)
-# docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
-# prompt = prompt.invoke({"question": question, "context": docs_content})
-# answer = llm.invoke(prompt)
+# question
+QUESTION = "Who is rank one in woman single?"
+response = graph.invoke({"question": QUESTION})
+print("Answer: ", response["answer"])
