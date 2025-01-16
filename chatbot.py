9f41e4666658825aad5c0e05e336258b3614be37
Winnie Liu <acs107102@gm.ntcu.edu.tw>
2025-01-16 23:13:31 +0000

    upload pdf + chat

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,14 @@
+import sys
+import io
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 import gradio as gr
 
 from dotenv import load_dotenv
 load_dotenv()
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 DATA_PATH = "Data"
 CHROMA_PATH = "Chroma_DB"
@@ -17,14 +22,13 @@ vector_store = Chroma(
     persist_directory=CHROMA_PATH,
 )
 
-num = 5
+num = 3
 retriever = vector_store.as_retriever(search_kwargs={'k': num})
 
 def streamResponse(message, history):
-    print(f"Input: {message}. History: {history}\n")
+    # print(f"Input: {message}. History: {history}\n")
 
     docs = retriever.invoke(message)
-    print(docs)
 
     knowledge = ""
 
@@ -56,13 +60,63 @@ def streamResponse(message, history):
         for response in llm.stream(rag_prompt):
             partial_message += response.content
             yield partial_message
+    
+    #     history.append({"role": "user", "content": message})
+    #     history.append({"role": "assistant", "content": partial_message})
+    #     print(partial_message)
+    # return "", history
+
+def process_pdf(file):
+    loader = PyPDFLoader(file)
+    docs = loader.load()
+
+    print(f"Loaded {len(docs)} documents from {file}")
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    all_splits = text_splitter.split_documents(docs)
+
+    print(f"Split the documents into {len(all_splits)} sub-documents")
+
+    vector_store.add_documents(documents=all_splits)
+
 
 # initiate the Gradio app
-chatbot = gr.ChatInterface(streamResponse, textbox=gr.Textbox(placeholder="Send to the LLM...",
-    container=False,
-    autoscroll=True,
-    scale=7),
-)
+with gr.Blocks() as chatbot:
+    with gr.Column():
+        upload_button = gr.File(label="Upload PDF")
+        submit_button = gr.Button("Submit")
+
+    output_area = gr.Textbox(label="Message", lines=1, interactive=False)
+
+    with gr.Column(visible=False) as chat_row:
+    #     cb = gr.Chatbot(type="messages",
+    #             container=False,
+    #             autoscroll=True,
+    #             scale=7)
+    #     msg = gr.Textbox(placeholder="Send to the LLM...",)
+    #     clear = gr.ClearButton([msg, cb])
+        
+    # msg.submit(streamResponse, [msg, cb], [msg, cb])
+
+        cb = gr.Chatbot(
+                placeholder="Send to the LLM...",
+                container=False,
+                autoscroll=True,
+                scale=7)
+        gr.ChatInterface(
+            streamResponse,
+            type="messages",
+            chatbot=cb)
+        clear = gr.ClearButton(cb)
+    
+    def handle_input(file):
+        if file:
+            process_pdf(file)
+            return "Upload Complete", gr.update(visible=True)
+        else:
+            return "Please upload file", gr.update(visible=False)
+
+    submit_button.click(handle_input, inputs=[upload_button], outputs=[output_area, chat_row])
 
 # launch the Gradio app
 chatbot.launch()
